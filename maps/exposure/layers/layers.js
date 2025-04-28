354346d8f4a8e9b99046585bc1dda8305fc22465
GeoFloodBagac <hcopunzalan@bpsu.edu.ph>
2025-04-29 03:23:56 +0800

    Patch 2.00

---
+++ maps/exposure/layers/layers.js
@@ -0,0 +1,42 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+var format_EXPOSURE_1 = new ol.format.GeoJSON();
+var features_EXPOSURE_1 = format_EXPOSURE_1.readFeatures(json_EXPOSURE_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_EXPOSURE_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_EXPOSURE_1.addFeatures(features_EXPOSURE_1);
+var lyr_EXPOSURE_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_EXPOSURE_1, 
+                style: style_EXPOSURE_1,
+                popuplayertitle: 'EXPOSURE',
+                interactive: true,
+    title: 'EXPOSURE<br />\
+    <img src="styles/legend/EXPOSURE_1_0.png" /> Very Low<br />\
+    <img src="styles/legend/EXPOSURE_1_1.png" /> Low<br />\
+    <img src="styles/legend/EXPOSURE_1_2.png" /> Moderate<br />\
+    <img src="styles/legend/EXPOSURE_1_3.png" /> High<br />\
+    <img src="styles/legend/EXPOSURE_1_4.png" /> Very High<br />' });
+
+lyr_GoogleSatellite_0.setVisible(true);lyr_EXPOSURE_1.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_EXPOSURE_1];
+lyr_EXPOSURE_1.set('fieldAliases', {'adm1_psgc': 'adm1_psgc', 'adm2_psgc': 'adm2_psgc', 'adm3_psgc': 'adm3_psgc', 'adm4_psgc': 'adm4_psgc', 'adm4_en': 'Barangay', 'geo_level': 'geo_level', 'len_crs': 'len_crs', 'area_crs': 'area_crs', 'len_km': 'len_km', 'area_km2': 'area_km2', 'h1': 'h1', 'h2': 'h2', 'E1': 'E1', 'E2': 'E2', 'V1': 'V1', 'C1': 'C1', 'C2': 'C2', 'E': 'E', 'H': 'H', 'V': 'V', 'C': 'C', 'R': 'R', 'R H': 'R H', 'R V': 'R V', 'R E': 'R E', 'R C': 'R C', 'R R': 'R R', });
+lyr_EXPOSURE_1.set('fieldImages', {'adm1_psgc': 'TextEdit', 'adm2_psgc': 'TextEdit', 'adm3_psgc': 'TextEdit', 'adm4_psgc': 'TextEdit', 'adm4_en': 'TextEdit', 'geo_level': 'TextEdit', 'len_crs': 'TextEdit', 'area_crs': 'TextEdit', 'len_km': 'TextEdit', 'area_km2': 'TextEdit', 'h1': 'TextEdit', 'h2': 'TextEdit', 'E1': 'TextEdit', 'E2': 'TextEdit', 'V1': 'TextEdit', 'C1': 'TextEdit', 'C2': 'TextEdit', 'E': 'TextEdit', 'H': 'TextEdit', 'V': 'TextEdit', 'C': 'TextEdit', 'R': 'TextEdit', 'R H': 'TextEdit', 'R V': 'TextEdit', 'R E': 'TextEdit', 'R C': 'TextEdit', 'R R': 'TextEdit', });
+lyr_EXPOSURE_1.set('fieldLabels', {'adm1_psgc': 'hidden field', 'adm2_psgc': 'hidden field', 'adm3_psgc': 'hidden field', 'adm4_psgc': 'hidden field', 'adm4_en': 'inline label - visible with data', 'geo_level': 'hidden field', 'len_crs': 'hidden field', 'area_crs': 'hidden field', 'len_km': 'hidden field', 'area_km2': 'hidden field', 'h1': 'hidden field', 'h2': 'hidden field', 'E1': 'hidden field', 'E2': 'hidden field', 'V1': 'hidden field', 'C1': 'hidden field', 'C2': 'hidden field', 'E': 'hidden field', 'H': 'hidden field', 'V': 'hidden field', 'C': 'hidden field', 'R': 'hidden field', 'R H': 'hidden field', 'R V': 'hidden field', 'R E': 'hidden field', 'R C': 'hidden field', 'R R': 'hidden field', });
+lyr_EXPOSURE_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ maps/flood-risk/styles/FLOODRISKINDEXMAP_1_style.js
@@ -0,0 +1,73 @@
+var size = 0;
+var placement = 'point';
+function categories_FLOODRISKINDEXMAP_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '5':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.704)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,37,115,0.704)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.704)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(90,0,254,0.704)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '3':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.704)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(175,69,255,0.704)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.704)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(227,210,255,0.704)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.704)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(247,229,255,0.704)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_FLOODRISKINDEXMAP_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("R R");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_FLOODRISKINDEXMAP_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
